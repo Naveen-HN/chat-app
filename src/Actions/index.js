908594404b1e9ad768fa9e7097d438d87f34a28b
Naveen-HN <hnnaveen93@gmail.com>
2020-02-25 13:15:39 -0500

    Added actions and reducers

---
+++ src/Actions/index.js
@@ -0,0 +1,29 @@
+import * as Types from "./types";
+
+let nextMessageID = 0;
+let nextUserID = 0;
+
+export const addMessage = (message, author) => ({
+  type: Types.ADD_MESSAGE,
+  id: nextMessageID++,
+  message,
+  author
+});
+
+export const addUser = name => ({
+  type: Types.ADD_USER,
+  id: nextUserID++,
+  name
+});
+
+export const messageReceived = (message, author) => ({
+  type: Types.MESSAGE_RECEIVED,
+  id: nextMessageID++,
+  message,
+  author
+});
+
+export const populateUsersList = users => ({
+  type: Types.USERS_LIST,
+  users
+});
